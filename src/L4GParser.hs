4f4faca36f21d0717a40f7e38033692d34b80d01
chocolatier <chocolatier@users.noreply.github.com>
2018-04-16 15:42:13 +1000

    stringy vocab parser

--- src/L4GParser.hs
+++ src/L4GParser.hs
@@ -37,11 +37,13 @@ oneOfSymbol x = choice $ fmap (symbol lexer) x
 
 cardinalitySpecParser = do
     symbol lexer "cardinality"
-    relation <- oneOfSymbol ["<", "=", "!=", ">"]
+    relation <- parseSizeRelation
     posint <- natural lexer
     if posint == 0 then 
         fail "Cardinality must be greater than 0" 
     else return $ show (relation, posint)
+
+parseSizeRelation = oneOfSymbol ["<", "=", "!=", ">"]
 
 sortSpecParser = cardinalitySpecListParser <|> enumerationParser <|> sortAliasParser 
 
@@ -58,3 +60,64 @@ sortAliasParser = (symbol lexer "alias") >> return <$> sortNameParser
 
 sortNameParser = symbol lexer "bool" <|> numSortNameParser <|> localSortNameParser
 
+vocabularyParser = many symbolDeclarationParser
+
+symbolDeclarationParser =   (symbol lexer "name" >> constantDeclarationListParser)
+                        <|> (symbol lexer "predicate" >> predicateDeclarationListParser)
+                        <|> (symbol lexer "function" >> functionDeclarationListParser)
+
+listParser f = ((:[]) <$> f) <|> (braces lexer $ many f)
+
+constantDeclarationListParser = listParser constantDeclarationParser
+
+predicateDeclarationListParser = listParser predicateDeclarationParser
+
+functionDeclarationListParser = listParser functionDeclarationParser
+
+constantDeclarationParser = do
+    constName <- localConstantNameParser 
+    symbol lexer ":"
+    sortName <- sortNameParser
+    specList <- functionSpecListParser
+    return (constName, [sortName], specList) 
+
+predicateDeclarationParser = do 
+    predName <- localPredicateNameParser
+    sortList <- parens lexer $ sepBy1 sortNameParser (comma lexer)
+    specList <- functionSpecListParser
+    return (predName, sortList, specList)
+
+functionDeclarationParser = do 
+    predName <- localFunctionNameParser
+    sortList <- parens lexer $ sepBy1 sortNameParser (comma lexer)
+    specList <- functionSpecListParser
+    return (predName, sortList, specList)
+    
+localConstantNameParser = identifier lexer
+
+localPredicateNameParser = identifier lexer 
+
+localFunctionNameParser = identifier lexer
+
+parsePredicateName = parseSizeRelation <|> oneOfSymbol ["EST","NOT","AND","OR","IMP"] <|> localPredicateNameParser
+
+--TODO: Should not drop the the sort name.
+functionNameParser = oneOfSymbol ["LEN", "DIF"] <|> (oneOfSymbol ["PRED", "SUCC", "+", "-"] >>= (\x -> optionalSortNameParser >> return x))
+
+--TODO: Should not drop the the sort name.
+constantParser = oneOfSymbol ["FALSE", "TRUE"] <|> (oneOfSymbol ["MAX", "MIN"] >>= (\x -> optionalSortNameParser >> return x)) <|> (show <$> natural lexer) <|> enumItemNameParser <|> localConstantNameParser
+
+--IDK if spaces are allowed to the left/right of the underscore. Assuming are.  
+optionalSortNameParser = try $ symbol lexer "_" >> sortNameParser
+
+
+functionSpecListParser = listParser functionSpecParser
+
+functionSpecParser = (\x -> (x,"")) <$> oneOfSymbol ["all_different", "surjective", "surjective", "total", "partial", "commutative", "priority", "no_priority", "hidden", "cut", "no_cut"]
+                     <|> scopeParser 
+                     <|> printParser 
+                     where
+                        scopeParser = symbol lexer "scope" >> (\x -> ("scope", show x)) <$> natural lexer 
+                        printParser = symbol lexer "print" >> try (colon lexer) >>  (\x -> ("print", show x)) <$> verbosityLevelParser
+
+verbosityLevelParser = oneOfSymbol ["none", "brief", "full"]
